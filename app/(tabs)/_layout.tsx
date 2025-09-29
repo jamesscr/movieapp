1585920bba386a01418b55345b18f596183ce9a1
jamesscr <allanjeanjacques@hotmail.com>
2025-09-29 08:37:19 -0400

    icons dynamic

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { ImageBackground, Image, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+
+// generalized ImgBacgraundComponent with props: any for now
+function TabIcon({ focused, icon, title }: any) {
+	if (focused) {
+		return (
+			<ImageBackground
+				source={images.highlight}
+				className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+			>
+				<Image source={icon} tintColor='#151312' className='size-5' />
+				<Text className='text-secondary text-base font-semibold ml-2'>
+					{title}
+				</Text>
+			</ImageBackground>
+		);
+	}
+
+	return (
+		<View className='size-full justify-center items-center mt-4 rounded-full'>
+			<Image source={icon} tintColor='#A8B5DB' className='size-5' />
+		</View>
+	);
+}
 
 const _layout = () => {
 	return (
@@ -9,6 +34,9 @@ const _layout = () => {
 				options={{
 					title: "Home",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<TabIcon focused={focused} icon={icons.home} title='Home' />
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -16,6 +44,9 @@ const _layout = () => {
 				options={{
 					title: "Search",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<TabIcon focused={focused} icon={icons.search} title='Search' />
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -23,6 +54,9 @@ const _layout = () => {
 				options={{
 					title: "Saved",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<TabIcon focused={focused} icon={icons.save} title='Saved' />
+					),
 				}}
 			/>
 			<Tabs.Screen
@@ -30,6 +64,9 @@ const _layout = () => {
 				options={{
 					title: "Profile",
 					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<TabIcon focused={focused} icon={icons.person} title='Profile' />
+					),
 				}}
 			/>
 		</Tabs>
