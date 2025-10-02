b71e81d86e95416eacc04782a58f306cea4216bb
jamesscr <allanjeanjacques@hotmail.com>
2025-10-02 12:07:47 -0400

    using tailwindcss

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 
 const Saved = () => {
@@ -10,5 +10,3 @@ const Saved = () => {
 };
 
 export default Saved;
-
-const styles = StyleSheet.create({});
