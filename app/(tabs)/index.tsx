f18d59fbb66ae1142ddec40a3b3f4480cfc802e2
jamesscr <allanjeanjacques@hotmail.com>
2025-10-02 12:03:43 -0400

    crt interface, MovieCard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,6 +13,7 @@ import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/usefetch";
 import { fetchMovies } from "@/services/api";
+import MovieCard from "../../components/MovieCard";
 
 export default function Index() {
 	// useRouter hook for router to move to diff screen
@@ -38,12 +39,43 @@ export default function Index() {
 			>
 				<Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
 
-				<SearchBar
-					onPress={() => {
-						router.push("/search");
-					}}
-					placeholder='Search for a movie'
-				/>
+				{moviesLoading ? (
+					<ActivityIndicator
+						size='large'
+						color='#0000ff'
+						className='mt-10 self-center'
+					/>
+				) : moviesError ? (
+					<Text>Error: {moviesError?.message}</Text>
+				) : (
+					<View className='flex-1 mt-5'>
+						<SearchBar
+							onPress={() => {
+								router.push("/search");
+							}}
+							placeholder='Search for a movie'
+						/>
+						<>
+							<Text className='text-lg text-white font-bold mt-5 mb-3'>
+								Latest Movies
+							</Text>
+							<FlatList
+								data={movies}
+								renderItem={({ item }) => <MovieCard {...item} />}
+								keyExtractor={(item) => item.id.toString()}
+								numColumns={3}
+								columnWrapperStyle={{
+									justifyContent: "flex-start",
+									gap: 20,
+									paddingRight: 5,
+									marginBottom: 10,
+								}}
+								className='mt-2 pb-32'
+								scrollEnabled={false}
+							/>
+						</>
+					</View>
+				)}
 			</ScrollView>
 		</View>
 	);
